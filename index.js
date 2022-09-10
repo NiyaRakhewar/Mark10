1b8e7f099e0fdfe44feca22eb645ac58951855b7
NiyaRakhewar <niharikarakhewar@gmail.com>
2022-09-10 18:07:23 +0530

    update

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const billAmount = document.querySelector("#bill-amount");
-const checkButton = console.log(billAmount.value);
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+// console.log(cashGiven.value);
+// console.log(billAmount.value);
+
+checkButton.addEventListener("click", function validateBillAndCashAmount(){
+    message.style.display = "none";
+    if(billAmount.value > 0){
+
+    }else{
+        message.style.display = "block";
+        message.innerText = " The bill amount sholud be greater than 0";
+    }
+})
